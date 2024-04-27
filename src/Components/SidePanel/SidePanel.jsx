54f0c1884c382136b13667ace7cbd3b24486ddbe
Viraj-Tharaka <virajtharaka97@gmail.com>
2024-04-27 16:42:17 +0530

    product page added

--- src/Components/SidePanel/SidePanel.jsx
+++ src/Components/SidePanel/SidePanel.jsx
@@ -5,42 +5,6 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./SidePanel.css";
 
-/*
-const SidePanel = () => {
-  return (
-    <div className="side-panel">
-      <h1 className="website-name">Self Checkout</h1>
-      <div className="buttons-section">
-        <MDBBtn block color="primary" className="mb-2">
-          <MDBIcon fas icon="tachometer-alt" className="me-2" />
-          Dashboard
-        </MDBBtn>
-        <MDBBtn block color="primary" className="mb-2">
-          <MDBIcon fas icon="box" className="me-2" />
-          Products
-        </MDBBtn>
-        <MDBBtn block color="primary" className="mb-2">
-          <MDBIcon fas icon="shopping-cart" className="me-2" />
-          Orders
-        </MDBBtn>
-        <MDBBtn block color="primary" className="mb-2">
-          <MDBIcon fas icon="users" className="me-2" />
-          Customers
-        </MDBBtn>
-      </div>
-      <div className="logout-section">
-        <MDBBtn block color="danger">
-          <MDBIcon fas icon="sign-out-alt" className="me-2" />
-          Log Out
-        </MDBBtn>
-      </div>
-    </div>
-  );
-};
-
-export default SidePanel;
-*/
-
 const SidePanel = () => {
   const [activeRoute, setActiveRoute] = useState(window.location.pathname);
   const [isHovered, setIsHovered] = useState(false);
--- src/pages/Products/Product.jsx
+++ src/pages/Products/Product.jsx
@@ -1,74 +1,174 @@
 import React, { useEffect, useState } from "react";
-import { Button, Table } from "react-bootstrap";
+import { Table, Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "./Products.css";
 
 function Product() {
   const [products, setProducts] = useState([]);
+  const [editProducts, setEditProducts] = useState(null);
 
   useEffect(() => {
     // Fetch data from the endpoint
-    fetch("http://localhost:5000/products/all")
+    fetch("http://localhost:8092/products/allProducts")
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const handleEdit = (product) => {
+    setEditProducts(product);
+  };
+
+  const handleSave = () => {
+
+    fetch(`http://localhost:8092/products/products/${editProducts.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(editProducts)
+    })
+      .then(response => {
+        if (response.ok) {
+          // Handle success
+          console.log(`product ${editProducts.id} updated successfully.`);
+          setEditProducts(null); // Clear editproduct after saving
+
+          // Fetch updated product list and set it as state to refresh the table
+          fetch("http://localhost:8092/products/allProducts")
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+            .catch((error) => console.error("Error fetching data:", error));
+        } else {
+          // Handle error
+          console.error(`Failed to update product ${editProducts.id}.`);
+        }
+      })
+      .catch(error => {
+        console.error('Error updating product:', error);
+      });
+  };
+
+
+  const handleDelete = (id) => {
+    // Implement delete functionality here
+    fetch(`http://localhost:8092/products/products/${id}`, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (response.ok) {
+          // Handle success
+          console.log(`product ${id} deleted successfully.`);
+          // Remove the deleted product from the state
+          setProducts(products.filter(product => product.id !== id));
+        } else {
+          // Handle error
+          console.error(`Failed to delete product ${id}.`);
+        }
+      })
+      .catch(error => {
+        console.error('Error deleting product:', error);
+      });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditProducts({ ...editProducts, [name]: value });
+  };
+
   return (
     <div className="main-cont1">
-      <h1>Product Page</h1>
+      <h1>Products</h1>
       <Link to="/addproduct">
-        <Button>Add Products</Button>
+        <Button>Add Product</Button>
       </Link>
       <br />
       <div className="table-container">
-        <Table bordered className="product-table">
-          <thead>
-            <tr>
-              <th>Product Name</th>
-              <th>Price</th>
-              <th>Stock</th>
-              <th>Image</th>
-              <th>Description</th>
-              <th>Barcode</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr key={product._id}>
-                <td>{product.productName}</td>
-                <td style={{ width: "80px" }}>$ {product.productPrice}0</td>
-                <td>{product.productStock}</td>
-                <td>
-                  <img
-                    src={product.imageUri}
-                    alt={product.productName}
-                    width="50"
-                  />
-                </td>
-                <td>{product.description}</td>
-                <td>{product.barcode}</td>
-                <td style={{ textAlign: "center", width: "100px" }}>
-                  <span
-                    role="img"
-                    aria-label="edit"
-                    style={{ cursor: "pointer", marginRight: "10px" }}
-                  >
-                    ✏️
-                  </span>
-                  <span
-                    role="img"
-                    aria-label="delete"
-                    style={{ cursor: "pointer" }}
-                  >
-                    🗑️
-                  </span>
-                </td>
+        {editProducts ? (
+          <div>
+            <h2>Edit Product</h2>
+            <Form>
+              <Form.Group controlId="formFirstName">
+                <Form.Label>Product Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter first name"
+                  name="pname"
+                  value={editProducts.pname}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formLastName">
+                <Form.Label>Price</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter last name"
+                  name="pamount"
+                  value={editProducts.pamount}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formEmail">
+                <Form.Label>Qty</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter email"
+                  name="quantity"
+                  value={editProducts.quantity}
+                  onChange={handleChange}
+                />
+              </Form.Group>
+
+              <Button variant="primary" onClick={handleSave}>
+                Save
+              </Button>
+              <Button variant="secondary" onClick={() => setEditProducts(null)}>
+                Cancel
+              </Button>
+            </Form>
+          </div>
+        ) : (
+          <Table bordered>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Product name</th>
+                <th>Price</th>
+                <th>QTY</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr key={product.id}>
+                  <td>{product.id}</td>
+                  <td>{product.pname}</td>
+                  <td>{product.pamount}</td>
+                  <td>{product.quantity}</td>
+                  <td style={{ textAlign: "center" }}>
+                    <span
+                      role="img"
+                      aria-label="edit"
+                      style={{ cursor: "pointer", marginRight: "10px" }}
+                      onClick={() => handleEdit(product)}
+                    >
+                      ✏️
+                    </span>
+                    <span
+                      role="img"
+                      aria-label="delete"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => handleDelete(product.id)}
+                    >
+                      🗑️
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </div>
     </div>
   );
